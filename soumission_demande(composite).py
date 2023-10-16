adfcbc157390298b4deed4fd1b639f9d8eb678df
nlsferrara@gmail.com <100352279+nlsferrara@users.noreply.github.com>
2023-10-17 00:13:00 +0200

    xml tentative

--- soumission_demande(composite).py
+++ soumission_demande(composite).py
@@ -1,17 +1,16 @@
 from suds.client import Client
 from suds import WebFault
-import json
 
 # Créez un client SOAP pour votre service
 url = "http://localhost:8000/ServiceExtractionInformation?wsdl"  # Assurez-vous d'ajuster l'URL de votre service WSDL
 client = Client(url)
 
-# data prend la valeur du JSON à envoyer 'johndoe.json'
-data = json.load(open('johndoe.json'))
+# data prend la valeur du XML à envoyer 'johndoe.xml'
+data = 'johndoe.xml'
 
 try:
     # Appelez la méthode du service en lui passant le JSON comme argument
-    response = client.service.ExtraireInformations(json.dumps(data))
+    response = client.service.ExtraireInformations(data)
 
     # Vérifiez la réponse du service
     print("Réponse du service:")
